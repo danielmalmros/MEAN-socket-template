62322693445842fa18f35c5c70e1079010390e63
Daniel Malmros Christiansen <daniel@bymalmros.dk>
2018-05-07 11:28:13 +0200

    added connection string

--- model/database.js
+++ model/database.js
@@ -3,7 +3,7 @@ var exports = module.exports = {};
 
 //Connect to database
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/test-socket-io', {useMongoClient: true});
+mongoose.connect('mongodb://danielmalmros:malmros12@ds117200.mlab.com:17200/socket-template', {useMongoClient: true});
 exports.db = mongoose.connection;
 exports.db.on('error', console.error.bind(console, 'connection error:'));
 exports.db.once('open', function() {
